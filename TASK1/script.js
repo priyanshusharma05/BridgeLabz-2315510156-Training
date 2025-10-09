49c7baa9b582ab54e6f56dfd598e3d129f282111
priyanshu sharma <sharmapriyanshu8865@gmail.com>
2025-10-09 10:31:58 +0530

    Initial upload to FULL-STACK branch

---
+++ TASK1/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click",function(){
+    alert("Button Clicked")
+})
+const button = document.getElementById("button");
+button.addEventListener("click", function() {
+    window.location.href = "Buttontrans.html";
+});
